9b61c6cac760d4dfb7f85483aa06f47a543d44e3
shahadat52 <shahadathossain.sh255@gmail.com>
2024-01-12 12:13:04 +0600

    project setup ,tailwind and shadcn setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-400 underline">
+      Hello world!
+    </h1>
+    <Button>My button</Button>
+    </>
+  )
+}
+
+export default App
